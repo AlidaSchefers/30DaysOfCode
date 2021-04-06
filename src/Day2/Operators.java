27b84158bd843be94b4c9434ba180fdee23777d9
AlidaSchefers <alidadschefers@gmail.com>
2021-04-06 14:24:00 -0400

    initial commit and solution

---
+++ src/Day2/Operators.java
@@ -0,0 +1,29 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Operators {
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double total_cost = meal_cost + (((double) tip_percent/100)*meal_cost) + (((double) tax_percent/100)*meal_cost);
+        System.out.println(Math.round(total_cost));
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+
+}
