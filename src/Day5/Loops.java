44a9cc7cbe5cee253ab90bdbaf2311128708ef76
AlidaSchefers <alidadschefers@gmail.com>
2021-04-07 14:01:00 -0400

    initial commit and solution

--- src/Day5/Loops.java
+++ src/Day5/Loops.java
@@ -1,2 +1,16 @@
-package Day5;public class Loops {
+package Day5;
+
+import java.util.Scanner;
+
+public class Loops {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(byte i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n*i);
+        }
+        scanner.close();
+    }
 }
