8f6cd349a584ab084d45035ed552a3762575cde2
AlidaSchefers <alidadschefers@gmail.com>
2021-06-21 21:53:45 -0400

    initial commit

---
+++ src/Day25_RunningTimeAndComplexity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Day25_RunningTimeAndComplexity {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        int tests = scanner.nextInt();
+        for(int i = 1; i <= tests; i++){
+            int testNum = scanner.nextInt();
+
+            System.out.println(testNum);
+        }
+    }
+}
